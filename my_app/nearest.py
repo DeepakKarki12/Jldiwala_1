48ed28510964db552dd22826c6be5fe0e875206c
Deepak Karki <88586113+DeepakKarki12@users.noreply.github.com>
2023-12-31 23:16:13 +0530

    Add files via upload

--- my_app/nearest.py
+++ my_app/nearest.py
@@ -15,12 +15,12 @@ class nearest_5:
 
     def web_driver(self):
         options = webdriver.ChromeOptions()
-        options.add_argument("--verbose")
-        options.add_argument('--no-sandbox')
+        # options.add_argument("--verbose")
+        # options.add_argument('--no-sandbox')
         options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=1920,1200')
-        options.add_argument('--disable-dev-shm-usage')
+        # options.add_argument('--disable-gpu')
+        # options.add_argument('--window-size=1920,1200')
+        # options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(options=options)
         # driver = webdriver.Chrome()
         return driver
@@ -55,15 +55,21 @@ class nearest_5:
 
             top = self.driver.find_elements('xpath',"(//a[@class='hfpxzc'])")
             print("top",top)
+            links = []
             for i in top[0:4]:
                 location_info = []
                 single_link = i.get_attribute("href")
+                links.append(single_link)
+            self.driver.quit()
+            for single_link in links:
                 self.driver1.get(single_link)
                 h1=self.driver1.find_element("tag name", "h1")
                 locate = self.driver1.find_elements('xpath',"(//div[@class='Io6YTe fontBodyMedium kR99db '])")
                 location_info.append(h1.text)
-                for i in locate:
-                    location_info.append(i.text)
+                # for i in locate:
+                #     location_info.append(i.text)
+                location_info.append(locate[0].text)
+                location_info.append(locate[1].text)
                 final_info.append(location_info)
                 print(final_info)
                 sleep(3)
