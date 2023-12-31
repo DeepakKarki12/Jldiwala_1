c20ecbf13cb5f4107218e29386a4936684c66c2d
Deepak Karki <88586113+DeepakKarki12@users.noreply.github.com>
2023-12-31 23:39:14 +0530

    nearest.py

--- my_app/nearest.py
+++ my_app/nearest.py
@@ -16,11 +16,11 @@ class nearest_5:
     def web_driver(self):
         options = webdriver.ChromeOptions()
         options.add_argument("--verbose")
-        options.add_argument('--no-sandbox')
+        # options.add_argument('--no-sandbox')
         options.add_argument('--headless')
         # options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=1920,1200')
-        options.add_argument('--disable-dev-shm-usage')
+        # options.add_argument('--window-size=1920,1200')
+        # options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(options=options)
         # driver = webdriver.Chrome()
         return driver
